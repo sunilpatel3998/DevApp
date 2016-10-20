e0af37fcbfaee871971279c00818436b4c38bd81
sunilpatel3998 <sunil.patel@hanusoftware.local>
2016-10-20 15:13:55 +0530

    Code

---
+++ DevApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DevApp.Startup))]
+namespace DevApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
